44b5db3f42ad5b908b9f72a43186469903e29726
dalwindr@gmail.com <dalwindr@gmail.com>
2022-06-19 02:03:57 +0530

    my first commit

--- pyzipcode/pyinferzipcode.py
+++ pyzipcode/pyinferzipcode.py
@@ -13,6 +13,7 @@ class Pyzipcode(object):
     def get(pincode: list, country_code=None, return_json=False):
         r = requests.get('https://raw.github.com/kennethreitz/requests/master/README.rst')
 
+
         # print("code", os.getcwd())
         # df = pd.read_csv("../pyzipecode/world_postal.zcsv", compression="zip")
         return df[pincode].to_dict("record")
