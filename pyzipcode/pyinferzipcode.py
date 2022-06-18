c7c096d02ab1be3fed1e0607656dbd05b9cc52d8
dalwindr@gmail.com <dalwindr@gmail.com>
2022-06-19 01:44:38 +0530

    my first commit

--- pyzipcode/pyinferzipcode.py
+++ pyzipcode/pyinferzipcode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c7bb4b098775db5e395cbb3e91cd82f3992fb5e6239c7a1cf2731f9faef78ce
-size 5480
+oid sha256:9df9011abe38c9593c75e690f45605970cd465d181229a3326ceb6d9227da069
+size 512
---
+++ tests/test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63b99e07ac3ab92b39d7a7d8d250060700d19488fd6482d688e857dff94301eb
+size 440
